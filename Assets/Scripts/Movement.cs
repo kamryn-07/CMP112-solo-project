ec9f67905abfe7213014b37399b0d48e9101d7f7
Kamryn <kamrynrobb07@gmail.com>
2025-12-15 10:38:05 +0000

    Rudimentary Movement System Implementation
    X and Z movement implemented with new Unity input system.
    
    Z movement is slower due to the architecture of the game's plans.

--- Assets/Scripts/Movement.cs
+++ Assets/Scripts/Movement.cs
@@ -1,16 +1,52 @@
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class Movement : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
+
+    // constants
+    const float SPEED_FACTOR_X = 0.075f;
+    const float SPEED_FACTOR_Y = 0.05f;
+
+    // public references
+    public GameObject player;
+
+    // variable initialisation
+
+        // objects/components
+    Rigidbody playerRigidbody;
+    InputAction moveAction;
+    
+        // regular data
+    Vector2 moveVector = Vector2.zero;
+    float xSpeed = 0f;
+    float ySpeed = 0f;
+
     void Start()
     {
-        
+
+        // component initialisation
+        playerRigidbody = player.GetComponent<Rigidbody>();
+
+        // action initialisation
+        moveAction = InputSystem.actions.FindAction("Move");
+
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
+
+        // read movement data
+        moveVector = moveAction.ReadValue<Vector2>();
+        xSpeed = moveVector.x * SPEED_FACTOR_X;
+        ySpeed = moveVector.y * SPEED_FACTOR_Y;
+
+        // apply movement to playerRigidbody
+        if (moveVector.magnitude > 0)
+        {
+            playerRigidbody.AddForce(xSpeed, 0, ySpeed, ForceMode.Impulse);
+        }
+
     }
+
 }
